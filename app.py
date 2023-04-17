de30656a6b765184561b1d6f67c79aaf9e31959a
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-04-17 10:47:45 +0500

    new

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    await db.drop_users()
     await db.create()
     # await db.drop_elements()
     await db.create_table_chanel()
