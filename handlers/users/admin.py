ddf99f7533ab875044c231907d4533790cc4814a
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-05-05 08:53:07 +0500

    new

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -14,37 +14,44 @@ from states.rekStates import RekData
 
 @dp.message_handler(text='Xabar Yuborish 🗒', user_id=ADMINS)
 async def bot_start(msg: types.Message, state: FSMContext):
-    await msg.answer("<b>Xabarni ni yuboring</b>")
+    await msg.answer("<b>Xabarni ni yuboring</b>", reply_markup=back)
     await RekData.choice.set()
 
 
 @dp.message_handler(content_types=['video', 'audio', 'voice', 'photo', 'document', 'text'], user_id=ADMINS,
                     state=RekData.choice)
 async def contumum(msg: types.Message, state: FSMContext):
-    if msg.video or msg.audio or msg.voice or msg.document or msg.photo or msg.text:
-
+    if msg.text == '🔙️ Orqaga':
+        await msg.answer('Bekor qilindi', reply_markup=admin_key)
         await state.finish()
 
-        users = await db.select_all_users()
-        count_baza = await db.count_users()
-        count_err = 0
-        count = 0
-        for user in users:
-            user_id = user[6]
-            try:
-                await msg.send_copy(chat_id=user_id)
-                count += 1
-                await asyncio.sleep(0.05)
+    elif msg.video or msg.audio or msg.voice or msg.document or msg.photo or msg.text:
+        if msg.text == 'Xabar Yuborish 🗒':
+            await msg.answer('Adashdingiz Shekilli\n\n'
+                             'To`g`ri ma`lumot kirting')
+        else:
+            await state.finish()
 
-            except Exception as err:
-                count_err += 1
-                await asyncio.sleep(0.05)
+            users = await db.select_all_users()
+            count_baza = await db.count_users()
+            count_err = 0
+            count = 0
+            for user in users:
+                user_id = user[3]
+                try:
+                    await msg.send_copy(chat_id=user_id)
+                    count += 1
+                    await asyncio.sleep(0.05)
 
-        await msg.answer(f"Ҳабар юборилганлар: <b>{count}</b> та."
-                         f"\n\nЮборилмаганлар: <b>{count_err}</b> та."
-                         f"\n\nБазада жами: <b>{count_baza}</b> та"
-                         f" фойдаланувчи мавжуд.", reply_markup=admin_key
-                         )
+                except Exception as err:
+                    count_err += 1
+                    await asyncio.sleep(0.05)
+
+            await msg.answer(f"Ҳабар юборилганлар: <b>{count}</b> та."
+                             f"\n\nЮборилмаганлар: <b>{count_err}</b> та."
+                             f"\n\nБазада жами: <b>{count_baza}</b> та"
+                             f" фойдаланувчи мавжуд.", reply_markup=admin_key
+                             )
 
 @dp.message_handler(state='*')
 async def back_all(message: types.Message, state: FSMContext):
