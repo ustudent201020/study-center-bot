7f4c80e9a6ece935b8fb7095bf92349c2246b971
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-06-05 07:18:32 +0500

    new

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("dasturchi", "Coder"),
+            # types.BotCommand("dasturchi", "Coder"),
             types.BotCommand("admin", "Faqat Adminlar uchun"),
             types.BotCommand("darslar", "Faqat Adminlar uchun"),
         ]
